688f125c34301f8010b42ec803ba7d008a1b85a2
G-Ark <ganesh.arkalgud@gmail.com>
2015-11-28 16:29:59 +0530

    Updating views as per updated URL

--- Store/views.py
+++ Store/views.py
@@ -15,23 +15,64 @@ class JSONResponse(HttpResponse):
         super(JSONResponse, self).__init__(content, **kwargs)
 
 # Create your views here.
-def index(request):
-	return HttpResponse("Index page of Store")
 
+# Main page of project at index
+def index(request):
+	return render(request, 'Store/index.html');
+
+# About page of project at about
+def about(request):
+	return render(request, 'Store/about.html');
+
+# About page of project at about
+def login(request):
+	return render(request, 'Store/works.html');
+
+# Contact page of project at contact
+def contact(request):
+	return render(request, 'Store/contact.html');
+
+# Lists json response of all the customers
 def customer(request):
 	if request.method == 'GET':
 		customer = Customer.objects.all()
 		serializer = CustomerSerializer(customer, many=True)
 		return JSONResponse(serializer.data)
 
+# Lists json response of a single customer at a time
+# Ex: /store/1/customer
+def customerDetails(request,customerId):
+	if request.method == 'GET':
+		customer = Customer.objects.get(pk=customerId)
+		serializer = CustomerSerializer(customer, many=False)
+		return JSONResponse(serializer.data)
+
+# Lists the json response of all the inventory items
 def inventory(request):
 	if request.method == 'GET':
 		inventory = Inventory.objects.all()
 		serializer = InventorySerializer(inventory, many=True)
 		return JSONResponse(serializer.data)
 
+# Lists json response of a single inventory at a time
+# Ex: /store/1/inventory
+def inventoryDetails(request,inventoryId):
+	if request.method == 'GET':
+		inventory = Inventory.objects.get(pk=inventoryId)
+		serializer = InventorySerializer(inventory, many=False)
+		return JSONResponse(serializer.data)
+
+# Lists the purcahses by all customers
 def purchase(request):
 	if request.method == 'GET':
 		purchase = Purchase.objects.all()
 		serializer = PurchaseSerializer(purchase, many=True)
+		return JSONResponse(serializer.data)
+
+# Lists json response of a single purchase at a time
+# Ex: /store/1/purchase
+def purchaseDetails(request,purchaseId):
+	if request.method == 'GET':
+		purchase = Purchase.objects.get(pk=purchaseId)
+		serializer = PurchaseSerializer(purchase, many=False)
 		return JSONResponse(serializer.data)
